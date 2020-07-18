dd8af98bdb7c9fb34295aebd22850aaec6bf7dc1
Jaysen-cod <applyindia24@gmail.com>
2020-07-18 15:00:57 +0530

    jaysen

---
+++ android/app/src/main/kotlin/com/example/prakat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prakat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/configs/agora_configs.dart
@@ -0,0 +1,1 @@
+const String APP_ID = "049935e8643e4027a8441c06e4eb2eed";
